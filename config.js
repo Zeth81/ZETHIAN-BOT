4a4d6ccc6fa283524f9a0091edc6803fba352e3f
Zeth81 <121651602+Zeth81@users.noreply.github.com>
2023-11-05 11:01:48 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -23,11 +23,11 @@ global.prems = []
 // EN ➜ Add the language code which GataBot will use
 //  es = Español      id = Bahasa Indonesia       ar = عرب
 //  en = English      pt = Português 
-global.lenguajeGB = es  //<-- Predeterminado en idioma Español 
+global.lenguajeGB = es //<-- Predeterminado en idioma Español 
 
 // confirmación en el ajuste de idioma y owner en la consola
-global.languageLen = ""
-global.registerNumber = ""
+global.languageLen = "es"
+global.registerNumber = "es"
 
 // ES ➜ Consigue Apikey en https://platform.openai.com/account/api-keys
 global.openai_key = 'sk-0'
